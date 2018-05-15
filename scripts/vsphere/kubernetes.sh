6538362070fc3369745c8746cfa0e1816017f425
Mohammed AbuAisha <mohammeda@cloudify.co>
2018-05-15 10:39:40 +0300

    Add support for multiple cni provider

--- scripts/vsphere/kubernetes.sh
+++ scripts/vsphere/kubernetes.sh
@@ -11,9 +11,23 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 
+
+cat <<EOF > /tmp/docker.repo
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
 sudo mv /tmp/kubernetes.repo /etc/yum.repos.d/kubernetes.repo
 sudo chmod 644 /etc/yum.repos.d/kubernetes.repo
 sudo chown root:root /etc/yum.repos.d/kubernetes.repo
+
+sudo mv /tmp/docker.repo /etc/yum.repos.d/docker.repo
+sudo chmod 644 /etc/yum.repos.d/docker.repo
+sudo chown root:root /etc/yum.repos.d/docker.repo
 
 sudo yum -y install deltarpm epel-release unzip
 
@@ -21,7 +35,7 @@ sudo yum makecache
 
 sudo setenforce 0
 
-sudo yum -y install kubelet-1.9.6-0 kubeadm-1.9.6-0 kubectl-1.9.6-0 kubernetes-cni-0.6.0-0 ca-certificates docker-1.12.6
+sudo yum -y install kubelet-1.9.6-0 kubeadm-1.9.6-0 kubectl-1.9.6-0 kubernetes-cni-0.6.0-0 ca-certificates docker-engine-1.12.6
 sudo update-ca-trust force-enable
 
 sudo groupadd docker
